8cb66bbcc04c730194fa29316315a5d5e7d479e5
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-07-17 18:27:53 +0545

    DrumKit With JavaScript

---
+++ DrumKit With JavaScript/script.js
@@ -0,0 +1,17 @@
+function playsound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    audio.currentTime = 0; 
+    audio.play();
+
+    key.classList.add('playing');
+
+    setTimeout(() => {
+        key.classList.remove('playing');
+    }, 500);
+}
+
+
+
+window.addEventListener('keydown', playsound );
